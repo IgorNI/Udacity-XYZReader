99b7768de643eab665fcb1a6af2fa8c8af752983
倪力烽 <nilf@senthink.com>
2017-04-26 22:43:45 +0800

    优化界面

--- XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
+++ XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
@@ -10,7 +10,8 @@ public class Config {
         URL url = null;
         try {
 //            url = new URL("https://dl.dropboxusercontent.com/u/231329/xyzreader_data/data.json" );
-            url = new URL("https://go.udacity.com/xyz-reader-json");
+//            url = new URL("https://go.udacity.com/xyz-reader-json");
+            url = new URL("https://raw.githubusercontent.com/TNTest/xyzreader/master/data.json");
         } catch (MalformedURLException ignored) {
             // TODO: throw a real error
         }
